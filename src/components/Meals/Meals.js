89a81a18a3ae39b8b95203874529c2a58d947266
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-02-03 11:32:07 +0300

    add meals component and css

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,17 @@
+
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+
+const Meals =()=>{
+
+    return(
+        <Fragment>
+
+        <MealsSummary />
+
+        </Fragment>
+    )
+
+}
+
+export default Meals;
