bcdaafcc01b07e960373dbe6dccb9ad5bb3d7df2
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-02-04 11:04:09 +0300

    add data provider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,8 +3,6 @@ import { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-import * as ACTION from './actions';
-
 
 const defaultCartState = {
     items: [],
@@ -15,7 +13,32 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
 
 
-    if (action.type ===ACTION.ADD) {
+    if (action.type === 'ADD') {
+
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const exisitingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+
+        const existingCartItem = state.items[exisitingCartItemIndex];
+
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+
+            updatedItems = [...state.items];
+
+            updatedItems[exisitingCartItemIndex] = updatedItem;
+
+        }
+        else {
+            updatedItems = state.items.concat(action.item);
+        }
 
         return {
             items: updatedItems,
@@ -24,6 +47,27 @@ const cartReducer = (state, action) => {
     }
 
     if (action.type === 'REMOVE') {
+
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+
+        const existingItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        }
+        else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+
+            updatedItems = [...state.items];
+
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
 
 
         return {
